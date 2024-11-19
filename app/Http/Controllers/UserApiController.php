1d5c61198810661b137b52a0eb22d796eef4892a
majdba123 <137396563+majdba123@users.noreply.github.com>
2024-11-19 14:49:52 +0300

    Autintication for Api
    The login function, the log out function, and the register function were built, where when you log in, a special token is generated for the user. Details are in the comments inside the UserApiController

---
+++ app/Http/Controllers/UserApiController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+class UserApiController extends Controller
+{
+    public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string'],
+            'email' => ['required', 'string', 'email', 'unique:users'],
+            'password' => ['required', 'min:8'],
+        ], [
+            'name.required' => 'Name is required',
+            'email.required' => 'Email is required',
+            'email.email' => 'Email is invalid',
+            'email.unique' => 'Email has already been taken',
+            'password.required' => 'Password is required',
+            'password.min' => 'Password must be at least 8 characters long',
+        ]);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors()->first();
+            return response()->json(['error' => $errors], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response()->json([
+            'message' => 'User Created ',
+        ]);
+    }
+
+
+
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email'=>'required|string|email',
+            'password'=>'required|min:8'
+        ], [
+            'email.required' => 'Email is required',
+            'email.email' => 'Email is invalid',
+            'password.required' => 'Password is required',
+            'password.min' => 'Password must be at least 8 characters long',
+        ]);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors()->first();
+            return response()->json(['error' => $errors], 422);
+        }
+
+        $user = User::where('email',$request->input('email'))->first();
+
+        if(!$user || !Hash::check($request->input('password'),$user->password)){
+            return response()->json([
+                'message' => 'Invalid Credentials'
+            ],401);
+        }
+
+        $token = $user->createToken($user->name.'-AuthToken')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+        ]);
+    }
+
+
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+
+        return response()->json([
+          "message"=>"logged out"
+        ]);
+    }
+}
